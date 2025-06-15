431230123c45bc3e70e09786e3be18b0289c4174
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-15 15:43:30 +0100

    Create index.php

---
+++ tasks from 06 to 12/task05/index.php
@@ -0,0 +1,15 @@
+<?php 
+    /* first method */
+    echo nl2br(<<< 'here'
+        Hello "'Elzero'"
+        We Love $Programming$
+        Languages Specially "PHP"
+    here
+);
+echo '<br> <br>';
+echo <<<'ha'
+    Hello "'Elzero'" <br>
+    We Love $Programming$ <br>
+    Languages Specially "PHP" <br>
+    ha;
+?>
