7deff0c19bff1a5ded7df3d673da866c638ae907
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-16 22:38:41 +0100

    Create task03.php

---
+++ Udemy challenge : Chap One/task03.php
@@ -0,0 +1,11 @@
+<?php
+/* write a php program to display structure in this order using escape sequence
+    Number   Square   Cube 
+    1        2          3
+    2        4          27
+*/
+
+echo "<pre>Number\tSquare\tCube</pre>";
+echo "<pre>1\t2\t3</pre>";
+echo "<pre>2\t4\t27</pre>";
+?>
