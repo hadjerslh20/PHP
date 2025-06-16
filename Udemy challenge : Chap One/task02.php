2c67aff0fc8360ac9733e8b181e57ee6bb1452c4
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-16 22:17:43 +0100

    Create task02.php

---
+++ Udemy challenge : Chap One/task02.php
@@ -0,0 +1,7 @@
+<?php
+// write a php program to make use of comments in hello world program
+    echo "Hello World";
+    // this is single line comment
+    # this is another single line comment
+    /* this is multiple line comment */
+?>
