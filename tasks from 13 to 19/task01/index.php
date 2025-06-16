eef5a6d7f327b1c4d7aa12bf482674eb3eb14a47
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-16 14:03:00 +0100

    Create index.php

---
+++ tasks from 13 to 19/task01/index.php
@@ -0,0 +1,21 @@
+<?php 
+    $owner = "Elzero";
+    $type = "Courses";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Elzero Courses">
+    <title>Welcome To Elzero Courses</title>
+  </head>
+  <body>
+    <h1><?php echo $owner.' '.$type; ?></h1>
+    <p>Here In <?php echo $owner.' '.$type; ?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?php echo $owner.' '.$type; ?> Is The What You Need.</div>
+    <footer>All Right Reserved To <?php echo $owner.' '.$type; ?></footer>
+  </body>
+</html>
