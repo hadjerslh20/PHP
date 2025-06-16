3819dd0c4e9e31ad25d9474392ffc78f3d016ece
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-16 14:44:32 +0100

    Create index.php

---
+++ tasks from 13 to 19/task06/index.php
@@ -0,0 +1,7 @@
+<?php
+    echo __LINE__; // to print lines
+    echo '<br>';
+    echo __FILE__; // to print file name and its path
+    echo '<br>';
+    echo __DIR__; // to print directory's file
+?>
