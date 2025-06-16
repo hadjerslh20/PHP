6416cba82a041dc17e08a8c248cc0289b017c673
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-16 14:11:51 +0100

    Create index.php

---
+++ tasks from 13 to 19/task02/index.php
@@ -0,0 +1,11 @@
+<?php 
+    $name = "elzero";
+    $$name = "Web";
+    echo  $$name;
+    echo '<br>';
+    echo  $elzero;
+    echo '<br>';
+    echo "{$elzero}";
+    echo '<br>';
+    echo "${$name}";
+?>
