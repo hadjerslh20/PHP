4783b2f0b8a69bdf60f7ea82b263094924322cff
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-16 20:12:08 +0100

    Create task01.php

---
+++ Udemy challenge : Chap One/task01.php
@@ -0,0 +1,16 @@
+<?php
+// write a php program to make use of all arithmetic operators in echo statement
+    $a=10;
+    $b=7;
+    echo 'Arithmetic operators';
+    echo '<br>';
+    echo 'Addition:', $a+$b;
+    echo '<br>';
+    echo 'Substraction:',$a-$b;
+    echo '<br>';
+    echo 'Multiplication:',$a*$b;
+    echo '<br>';
+    echo 'Division:',$a/$b;
+    echo '<br>';
+    echo 'Modulud:',$a%$b;
+?>
