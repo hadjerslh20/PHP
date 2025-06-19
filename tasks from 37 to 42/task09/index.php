7314e7280886d5755fcdedd5ca63c7aec5539c34
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-19 23:26:42 +0100

    Create index.php

---
+++ tasks from 37 to 42/task09/index.php
@@ -0,0 +1,9 @@
+<?php 
+    $help_num = 3;
+    $nums = [4, 5, 6, 1, 2, 3];
+    $names = ["Ahmed", "Sayed", "Osama", "Mahmoud", "Gamal"];
+    for($i=1; $i<$help_num; $i++){
+        echo $names[$i];
+        echo '<br>';
+    }
+?>
