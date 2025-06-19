9f2f1cfae6f35dc7e8f4294479bebc0050f3b480
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-19 23:09:56 +0100

    Create index.php

---
+++ tasks from 37 to 42/task06/index.php
@@ -0,0 +1,7 @@
+<?php 
+    $money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
+    foreach($money as $name => $quantity) {
+        echo "The Name Is $name And I Need $quantity Pound From Him";
+        echo '<br>';
+    }
+?>
