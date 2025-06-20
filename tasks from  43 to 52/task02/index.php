38700b2dd7a47b57dca3916ffc9365f5a8c9598c
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-20 21:04:31 +0100

    Update index.php

--- tasks from  43 to 52/task02/index.php
+++ tasks from  43 to 52/task02/index.php
@@ -1,11 +1,26 @@
 <?php 
-
-    function get_arguments(...$args){
+    // method one
+    function get_argumentsOne(...$args){
         $all='';
         foreach($args as $item) {
             $all .= $item.' ';  
         }
+        /*print_r(func_get_args());
+        echo '<br>';
+        print_r($args);
+        echo '<br>';*/
         return $all;
     }
-    echo get_arguments("Hello", "Elzero", "Web", "School");
+    echo get_argumentsOne("Hello", "Elzero", "Web", "School");
+    echo '<br>';
+    // method two 
+    function get_argumentsTwo() {
+        $all ='';
+        foreach(func_get_args() as $item ):
+            $all .= $item.' ';
+        endforeach;
+        return $all;
+    }
+    echo get_argumentsTwo("I", "Love", "PHP");
+    
 ?>
