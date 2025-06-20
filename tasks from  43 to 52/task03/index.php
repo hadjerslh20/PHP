2a0a4e38e02a8856fbc966eafee89e60b930a97a
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-20 21:19:13 +0100

    Update index.php

--- tasks from  43 to 52/task03/index.php
+++ tasks from  43 to 52/task03/index.php
@@ -1,1 +1,21 @@
 
+<?php 
+    function sum_all(...$nums) {
+        $sum=0;
+        foreach($nums as $num):
+            if($num==5){
+                continue;
+            }elseif($num==10){
+                $num=20;
+                $sum+=$num;
+            } else {
+                $sum+=$num;
+            }
+        endforeach;
+        return $sum;
+
+    }
+    echo sum_all(10, 12, 5, 6, 6, 10);
+    echo '<br>';
+    echo sum_all(5, 10, 5, 10);
+?>
