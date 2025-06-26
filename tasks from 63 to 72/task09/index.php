80e04353b71f9762378668d87006115fbfa2a531
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-26 22:49:02 +0100

    Create index.php

---
+++ tasks from 63 to 72/task09/index.php
@@ -0,0 +1,9 @@
+<?php
+    $nums = [1, 2, 3, 4, 5, 6];
+    echo '<pre>';
+    print_r(array_slice($nums,1,3));
+    echo '</pre>';
+    echo '<pre>';
+    print_r(array_slice($nums,-5,3));
+    echo '</pre>';
+?>
