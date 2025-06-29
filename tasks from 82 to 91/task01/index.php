4fee099c94debf1896180bee6fe1be3901ed294d
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-29 18:18:04 +0100

    Update index.php

--- tasks from 82 to 91/task01/index.php
+++ tasks from 82 to 91/task01/index.php
@@ -1,3 +1,5 @@
 <?php 
     echo disk_total_space("c:/")/(1024**4)."=".round(disk_total_space("c:/")/(1024**4))." Terabyte";
+    echo "<br>";
+    echo number_format(disk_total_space("c:/")/(1024**4),2);
 ?>
