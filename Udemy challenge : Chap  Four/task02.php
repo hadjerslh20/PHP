6398023d30541f187c44ff89cc0b334780fbf019
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-30 01:13:00 +0100

    Create task02.php

---
+++ Udemy challenge : Chap  Four/task02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task02</title>
+</head>
+<body>
+    <form method="POST">
+        <label>your text:</label>
+        <input type="text" name="text"> <br><br>
+        <input type="submit" value="Send">
+    </form>
+    <?php 
+        if($_SERVER['REQUEST_METHOD']=== 'POST') {
+            $text = $_POST['text'];
+            $textword= str_word_count($text);
+            echo "your text size is : $textword";
+        }
+    ?>
+</body>
+</html>
+
