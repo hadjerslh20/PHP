87b0ccb8d8a25554b2cbe1d37140d427fc781b09
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-06-30 22:29:29 +0100

    Create task03.php

---
+++ Udemy challenge : Chap Five/task03.php
@@ -0,0 +1,16 @@
+<?php 
+    $array=[5,-10,17,27,07];
+    $max= max($array);
+    echo "the max of:";
+    echo "<br>";
+    echo "<pre>";
+    print_r($array);
+    echo "</pre> is: $max".'<br>';
+    $min= min($array);
+    echo "the max of:";
+    echo "<br>";
+    echo "<pre>";
+    print_r($array);
+    echo "</pre> is: $min".'<br>';
+?>
+
