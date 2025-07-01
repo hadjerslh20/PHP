25c8f1b0ca1d24042fc34beb45b51bdf2437fee8
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-01 21:26:13 +0100

    Create index.php

---
+++ tasks from 98 to 105/task01/index.php
@@ -0,0 +1,5 @@
+<?php 
+    setcookie("site[layout]","boxed",strtotime("+ 2months 5days"),"/");
+    setcookie("site[font]","swat",strtotime("+ 2months 5days"),"/");
+    setcookie("site[color]","blue",strtotime("+ 2months 5days"),"/");
+?>
