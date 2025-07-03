287de32893b7e294c063d2b788c4ac2dae6799d8
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-03 21:37:28 +0100

    Create task05.php

---
+++ Udemy challenge : Chap eight/task05.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task05</title>
+</head>
+<body>
+    <form method="GET">
+        <label>Number One:</label>
+        <input type="number" name="numOne"><br><br>
+        <label>Number Two:</label>
+        <input type="number" name="numTwo"><br><br>
+        <input type="submit" value="Send">
+    </form>
+    <?php 
+if($_SERVER['REQUEST_METHOD']==="GET" && isset($_GET['numOne']) && isset($_GET['numTwo'])){
+    $start = $_GET['numOne'];
+    $end = $_GET['numTwo'];
+    $i=$start;
+    echo "Numbers between $start and $end are:"."<br>";
+    while( $i<=$end ) {
+        echo $i;
+        echo '<br>';
+        $i++;
+    }
+}
+?>
+</body>
+</html>
