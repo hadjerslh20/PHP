74f36b909bf7b113108498d4fa6c42b46226db8b
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-03 21:59:38 +0100

    Create task01.php

---
+++ Udemy challenge : Chap nine/task01.php
@@ -0,0 +1,10 @@
+<?php  
+    function math($a,$b){
+        echo "$a + $b = ".$a+$b."<br>";
+        echo "$a - $b = ".$a-$b."<br>";
+        echo "$a * $b = ".$a*$b."<br>";
+        echo "$a / $b = ".$a/$b."<br>";
+        echo "$a % $b = ".$a%$b."<br>";
+    }
+    math(27,17);
+?>      
