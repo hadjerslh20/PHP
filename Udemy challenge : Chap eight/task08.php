c6bc88e1d4032c2fec5f075c5cb0d88b7c87c79e
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-03 21:48:00 +0100

    Create task08.php

---
+++ Udemy challenge : Chap eight/task08.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task08</title>
+</head>
+<body>
+    <form method="GET">
+        <label>Number One:</label>
+        <input type="number" name="numOne"><br><br>
+        <input type="submit" value="Send">
+    </form>
+    <?php 
+if($_SERVER['REQUEST_METHOD']==="GET" && isset($_GET['numOne'])){
+    $start = $_GET['numOne'];
+    $i=1;
+    echo "Numbers of $start are:"."<br>";
+    do{
+        echo $i;
+        echo '<br>';
+        $i+=$start;
+    }while($i <=100);
+}
+?>
+</body>
+</html>
