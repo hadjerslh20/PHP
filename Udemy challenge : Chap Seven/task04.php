691cd356b800e4e09b863a0d0bd1f1f362d04f6f
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-07-03 02:52:07 +0100

    Update task04.php

--- Udemy challenge : Chap Seven/task04.php
+++ Udemy challenge : Chap Seven/task04.php
@@ -1,36 +1,46 @@
+<?php 
+    if($_SERVER['REQUEST_METHOD']=="POST" && isset($_POST["numOne"]) && isset($_POST["numTwo"])) {
+        $numOne= $_POST["numOne"];
+        $numTwo= $_POST["numTwo"];
+        if($numOne == $numTwo) {
+            $verify= true;
+        }else{
+            $error= "These numbers are not equal !"; 
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task04</title>
+    <title>task01</title>
 </head>
 <body>
-    <form method="POST">
-        <label>Number 1:</label>
-        <input type="number" name="numOne"> <br><br>
-        <label>Number 2:</label>
-        <input type="number" name="numTwo"> <br><br>
+    <form method = "POST">
+        <label>Number One:</label>
+        <input type="number" name="numOne" placeholder="Enter number One"> <br><br>
+        <label>Number Two:</label>
+        <input type="number" name="numTwo" placeholder="Enter number Two"> <br><br>
         <input type="submit" value="Check">
     </form>
     <?php 
-        if($_SERVER['REQUEST_METHOD']=="POST" && isset($_POST["numOne"])&& isset($_POST["numTwo"])) {
-            $num1=$_POST["numOne"];
-            $num2=$_POST["numTwo"];
-            if($num1 == $num2) { ?>
-                <form method="GET">
-                <label>Name:</label>
-                <input type="text" name="username" placeholder="Enter your name"> <br><br>
-                <input type="submit" value="send" name="send">
-                </form>
-            <?php 
-            if(isset($_GET['send'])){
-                $name=$_GET['username'];
-                echo "Hello ".$name; }
-            }else {
-                echo "Your numbers are not equal";
-            }
+        if(isset($verify)) {?>
+        <form method = "GET">
+        <label>username:</label>
+        <input type="text" name="username" placeholder="Enter your name"> <br><br>
+        <input type="submit" value="show">
+        </form>
+        <?php 
+        if($_SERVER['REQUEST_METHOD']=="GET" && isset($_GET["username"])) {
+            $username = $_GET["username"];
+            echo "Hello $username";
         }
-    ?>
+    
+}elseif(isset($error)) {
+        echo $error;
+    } ?>
+
 </body>
 </html>
+
